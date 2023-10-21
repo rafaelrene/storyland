a41d4880360a1bf212a01e6a257bb3c66dc03cbb
René Rafael <rene.rafael@groupsolver.com>
2023-10-21 12:33:20 +0200

    BATMAN!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const EditorJS = (await import('@editorjs/editorjs')).default;
+		const Header = (await import('@editorjs/header')).default;
+
+		const editor = new EditorJS({
+			holder: 'editor',
+			placeholder: "Let's write an awesome story",
+
+			tools: {
+				header: Header
+			}
+		});
+	});
+</script>
+
+<div id="editor" />
